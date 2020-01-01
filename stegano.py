f3bdc735d2f29ffe8d8c7033a0bc9d74d4219856
EmilioRivera <emilio.rivera@polymtl.ca>
2020-01-01 03:35:29 -0500

    Add option n to specify number of bits to use

--- stegano.py
+++ stegano.py
@@ -1,20 +1,30 @@
 import click
 from PIL import Image, ImageMath
 
-def _merge(img1, img2):
+MASKS = {
+    1: 0b10000000,
+    2: 0b11000000,
+    3: 0b11100000,
+    4: 0b11110000,
+    5: 0b11111000,
+    6: 0b11111100,
+    7: 0b11111110,
+}
+
+def _merge(img1, img2, n=4):
     r1, g1, b1 = img1.split()
     r2, g2, b2 = img2.split()
-    o_r = ImageMath.eval("(a & 0xF0) + (b >> 4) ", a=r1, b=r2).convert('L')
-    o_g = ImageMath.eval("(a & 0xF0) + (b >> 4) ", a=g1, b=g2).convert('L')
-    o_b = ImageMath.eval("(a & 0xF0) + (b >> 4) ", a=b1, b=b2).convert('L')
+    o_r = ImageMath.eval("(a & m) + (b >> n) ", a=r1, b=r2, m=MASKS[8-n], n=8-n).convert('L')
+    o_g = ImageMath.eval("(a & m) + (b >> n) ", a=g1, b=g2, m=MASKS[8-n], n=8-n).convert('L')
+    o_b = ImageMath.eval("(a & m) + (b >> n) ", a=b1, b=b2, m=MASKS[8-n], n=8-n).convert('L')
     output = Image.merge("RGB", (o_r, o_g, o_b))
     return output
 
-def _unmerge(img):
+def _unmerge(img, n=4):
     r, g, b = img.split()
-    o_r = ImageMath.eval(" (a << 4) & 0xF0 ", a=r).convert('L')
-    o_g = ImageMath.eval(" (a << 4) & 0xF0 ", a=g).convert('L')
-    o_b = ImageMath.eval(" (a << 4) & 0xF0 ", a=b).convert('L')
+    o_r = ImageMath.eval(" (a << n) & m ", a=r, m=MASKS[n], n=8-n).convert('L')
+    o_g = ImageMath.eval(" (a << n) & m ", a=g, m=MASKS[n], n=8-n).convert('L')
+    o_b = ImageMath.eval(" (a << n) & m ", a=b, m=MASKS[n], n=8-n).convert('L')
     output = Image.merge("RGB", (o_r, o_g, o_b))
     return output
 
@@ -28,16 +38,18 @@ def cli():
 @click.option('--img1', required=True, type=str, help='Image that will hide another image')
 @click.option('--img2', required=True, type=str, help='Image that will be hidden')
 @click.option('--output', required=True, type=str, help='Output image')
-def merge(img1, img2, output):
-    merged_image = _merge(Image.open(img1), Image.open(img2))
+@click.option('-n', type=int, help='Number of bits to use')
+def merge(img1, img2, output, n):
+    merged_image = _merge(Image.open(img1), Image.open(img2), n)
     merged_image.save(output)
 
 
 @cli.command()
 @click.option('--img', required=True, type=str, help='Image that will be hidden')
 @click.option('--output', required=True, type=str, help='Output image')
-def unmerge(img, output):
-    unmerged_image = _unmerge(Image.open(img))
+@click.option('-n', type=int, help='Number of bits to use')
+def unmerge(img, output, n):
+    unmerged_image = _unmerge(Image.open(img), n)
     unmerged_image.save(output)
 
 
