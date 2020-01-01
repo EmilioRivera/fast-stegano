0526a1049d650a894e0b3bd3aae76c5c7414c079
EmilioRivera <emilio.rivera@polymtl.ca>
2020-01-01 03:40:15 -0500

    Simple script to quickly test input values of n

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+MSB_VALUES=( 1 2 3 4 5 6 7 )
+INPUT_FILE="${INPUT_FILE:-input.jpg}"
+CONCEAL_FILE="${CONCEAL_FILE:-toconceal.jpg}"
+
+mkdir -p 'test'
+
+# TODO: Put correct venv path or comment if not needed
+source './.stegano/bin/activate'
+
+for n in "${MSB_VALUES[@]}"; do
+    echo "$n"
+    python stegano.py merge --img1 "$INPUT_FILE" --img2 "$CONCEAL_FILE" --output "test/concealed_${n}.png" -n "$n"
+    python stegano.py unmerge --img "test/concealed_${n}.png" --output "test/reconstructed_${n}.png" -n "$n"
+done
